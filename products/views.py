84a3dade8f857cb9bf70f0fa8a570e445d1d45e8
jamesr1775 <james.rooney@ucdconnect.ie>
2021-11-13 13:54:26 +0000

    adding products.html template, hooked up the apps url and made a simple view for it

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
+from .models import Product
 
-# Create your views here.
+def all_products(request):
+    """ View to return the products page """
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
