6ff478b78922b369ba199bace04bb9b452132363
jamesr1775 <james.rooney@ucdconnect.ie>
2021-11-17 18:52:23 +0000

    Added initial reviews and others purchased sections to the product details page, updated the model to have other products people purchased with default value

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 class Category(models.Model):
 
@@ -21,14 +21,20 @@ class Product(models.Model):
         LOWSTOCK = "Low Stock",
         INSTOCK = "In Stock",
         OUTOFSTOCK = "Out of Stock"
+
+    def get_default_products_others_bought():
+        default_products_others_bought = ["1", "5", "10", "15"]
+        return str("_".join(default_products_others_bought))
+
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    product_rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)    
+    product_rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     product_type = models.CharField(max_length=254, null=True, blank=True)
+    products_others_bought = models.CharField(max_length=254, default=get_default_products_others_bought, null=True, blank=True)
     status = models.CharField(max_length=254, choices=Product_status.choices, default=Product_status.INSTOCK)
     image = models.ImageField(null=True, blank=True)
 
--- products/views.py
+++ products/views.py
@@ -14,10 +14,16 @@ def all_products(request):
 def product_detail(request, product_id):
     """ View to show a products details """
     product = get_object_or_404(Product, pk=product_id)
+    others_bought_ids = list(product.products_others_bought.split("_"))
+    products_others_bought = []
+
+    for pid in others_bought_ids:
+        products_others_bought.append(get_object_or_404(Product, pk=pid))
     context = {
         'product': product,
         'star_loop': range(1, 6),
         'product_quantity_loop': range(1, 21),
+        'products_others_bought': products_others_bought,
     }
 
     return render(request, 'products/product_details.html', context)
