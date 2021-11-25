803612e994600427128f47075e4c1bfe44e6e547
jamesr1775 <james.rooney@ucdconnect.ie>
2021-11-25 20:13:31 +0000

    Created the template and view for the checkout page, updated proceed to checkout button to bring user to checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import OrderForm
 
-# Create your views here.
+def checkout(request):
+
+    shopping_bag = request.session.get('shopping_bag', {})
+    order_form = OrderForm()
+
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
