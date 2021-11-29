68defed3f2cb8051fe1f1cfae2fe227e9e8504c1
jamesr1775 <james.rooney@ucdconnect.ie>
2021-11-29 17:44:43 +0000

    added webhook handling and caching user form data for later processing

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from django.conf import settings
+from django.views.decorators.http import require_POST
 from .forms import OrderForm
 from django.contrib import messages
 from products.models import Product
@@ -7,6 +8,24 @@ from .models import OrderLineItem, Order
 from shopping_bag.contexts import shopping_bag_contents
 import stripe
 import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'shopping_bag': json.dumps(request.session.get('shopping_bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Error, your payment cannot be \
+            processed right now. Try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
