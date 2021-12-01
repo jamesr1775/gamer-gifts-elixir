6d5428303723abc8538c549c7847ad95f21931e4
jamesr1775 <james.rooney@ucdconnect.ie>
2021-12-01 04:55:03 +0000

    updated the user profile template, added profile info form, renamed field in userprofile model

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
     """ View to view user profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(request.POST, instance=profile)
+    orders = profile.orders.all()
+    print(profile)
+    print(orders)
     template = 'profiles/profile.html'
+
     context = {
+        'form': form,
+        'orders': orders,
     }
     return render(request, template, context)
