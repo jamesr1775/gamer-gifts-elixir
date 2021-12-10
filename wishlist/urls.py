2abb56354b38cc8fcff8e505c946611880303e7f
jamesr1775 <james.rooney@ucdconnect.ie>
2021-12-10 02:07:12 +0000

    remove product from wishlist added, added buttons to remove item from wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
-    path('add_product_to_wishlist/<int:product_id>/', views.add_product_to_wishlist, name='add_product_to_wishlist'),
+    path(
+        'add_product_to_wishlist/<int:product_id>/',
+        views.add_product_to_wishlist,
+        name='add_product_to_wishlist'),
+    path(
+        'remove_product_from_wishlist/<int:product_id>/',
+        views.remove_product_from_wishlist,
+        name='remove_product_from_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from products.models import Product
-from products.models import UserProfile
+from profiles.models import UserProfile
 from .models import WishList
+
 
 def wishlist(request):
     """ View to return the users Wishlist page """
     profile = UserProfile.objects.get(user=request.user)
-    users_wishlist =  WishList.objects.filter(user_profile=profile)
-    print(users_wishlist)
-
+    users_wishlist = WishList.objects.filter(user_profile=profile)
     products = []
     for item in users_wishlist:
         products.append(item.product)
 
     context = {
         'products': products,
+        'star_loop': range(1, 6),
     }
     return render(request, 'wishlist/wishlist.html', context)
 
@@ -23,10 +23,15 @@ def add_product_to_wishlist(request, product_id):
     """ View to add to users wishlist """
     profile = UserProfile.objects.get(user=request.user)
     product = get_object_or_404(Product, pk=product_id)
-    wishlist_item = WishList.objects.get_or_create(user_profile=profile, product=product)
-    users_wishlist = WishList.objects.filter(user_profile=profile)
-    # Add product to the wishlist
-    context = {
-        'products': users_wishlist,
-    }
+    WishList.objects.get_or_create(
+        user_profile=profile, product=product)
+    return redirect(reverse('product_detail', args=[product.id]))
+
+
+def remove_product_from_wishlist(request, product_id):
+    """ View to remove product from users wishlist """
+    profile = UserProfile.objects.get(user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+    wishlist = WishList.objects.get(product=product, user_profile=profile)
+    wishlist.delete()
     return redirect(reverse('wishlist'))
